3f78a27c710a8ea92430429533a3bda8a1aa7b56
Triongle <triongle@mac.home>
2025-09-12 16:59:47 +0100

    Simplify Proxy Store dispatchEvent

--- services/store.js
+++ services/store.js
@@ -4,16 +4,13 @@ const Store = {
 };
 
 const proxiedStore = new Proxy(Store, {
+  // The 'set' trap - runs whenever you do proxiedStore[property] = value
   set(target, property, value) {
     const prev = { ...target };
     const ok = Reflect.set(target, property, value);
     const next = { ...target };
 
-    window.dispatchEvent(
-      new CustomEvent("appstatechange", {
-        detail: { prev, next, patch: { [property]: value } },
-      })
-    );
+    window.dispatchEvent(new Event("appstatechange"));
     return ok;
   },
 });
