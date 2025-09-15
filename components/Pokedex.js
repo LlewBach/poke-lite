87a5938c1676b33f89411e2b4d771837622900ad
Triongle <triongle@mac.home>
2025-09-15 14:11:38 +0100

    Add Pokedex search

--- components/Pokedex.js
+++ components/Pokedex.js
@@ -1,6 +1,7 @@
 export default class Pokedex extends HTMLElement {
   constructor() {
     super();
+    this.pokeIndex = null;
     this.limit = 24;
     this.page = this._readPageFromHash() || 1;
     this.items = [];
@@ -57,43 +58,118 @@ export default class Pokedex extends HTMLElement {
     });
   }
 
-  async load() {
-    const offset = (this.page - 1) * this.limit;
-    // this.querySelector("#status").textContent = "Loading...";
-    try {
-      const res = await fetch(
-        `https://pokeapi.co/api/v2/pokemon?limit=${this.limit}&offset=${offset}`
-      );
-      if (!res.ok) throw new Error(`HTTP ${res.status}`);
-      const list = await res.json();
-      this.count = list.count;
-      console.log("List: ", list);
+  _bindSearch() {
+    const form = this.querySelector("#search-form");
+    const input = this.querySelector("#search-input");
+    const clearBtn = this.querySelector("#clear-search");
 
-      const details = await Promise.all(
-        list.results.map((animal) => fetch(animal.url).then((r) => r.json()))
-      );
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.search = input.value.trim().toLowerCase();
+      if (this.search) {
+        this.load();
+      }
+    });
 
-      console.log(details);
+    clearBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (!this.search) return;
+      this.search = "";
+      this.load();
+    });
+  }
 
-      this.items = details.map((d) => ({
-        pid: d.id,
-        name: d.name,
-        img:
-          d.sprites?.other?.["official-artwork"]?.front_default ||
-          d.sprites?.front_default ||
-          "",
-        types: d.types.map((t) => t.type.name),
-        stats: Object.fromEntries(
-          d.stats.map((s) => [s.stat.name, s.base_stat])
-        ),
-      }));
-      this.render();
-      this._bindPager();
-    } catch (err) {
-      this.innerHTML = `<p style="color:red">Error loading Pokédex: ${String(
-        err
-      )}</p>`;
+  async ensureIndex() {
+    // Cache hit
+    if (this.pokeIndex) {
+      console.log("Hitting cache!");
+      return this.pokeIndex;
     }
+
+    // Start fetch
+    console.log("Fetching pokeIndex");
+    const res = await fetch(
+      "https://pokeapi.co/api/v2/pokemon?limit=2000&offset=0"
+    );
+    if (!res.ok) throw new Error(`HTTP ${res.status}`);
+    const data = await res.json();
+
+    this.pokeIndex = data.results.map((r) => {
+      const id = r.url.match(/\/pokemon\/(\d+)\//)[1];
+      return { pid: id, name: r.name, url: r.url };
+    });
+  }
+  async load() {
+    if (this.search) {
+      // Search load
+      try {
+        await this.ensureIndex();
+        const searchUrl = this.pokeIndex.find(
+          (p) => p.pid == this.search || p.name == this.search
+        ).url;
+        if (searchUrl) {
+          const urlRes = await fetch(searchUrl);
+          if (!urlRes.ok) throw new Error(`HTTP ${urlRes.status}`);
+          const data = await urlRes.json();
+          this.items = [
+            {
+              pid: data.id,
+              name: data.name,
+              img:
+                data.sprites?.other?.["official-artwork"]?.front_default ||
+                data.sprites?.front_default ||
+                "",
+              types: data.types.map((t) => t.type.name),
+              stats: Object.fromEntries(
+                data.stats.map((s) => [s.stat.name, s.base_stat])
+              ),
+            },
+          ];
+        }
+      } catch (err) {
+        this.innerHTML = `<p style="color:red">Error finding Pokémon: ${String(
+          err
+        )}</p>`;
+      }
+    } else {
+      // Full load
+      const offset = (this.page - 1) * this.limit;
+      try {
+        const res = await fetch(
+          `https://pokeapi.co/api/v2/pokemon?limit=${this.limit}&offset=${offset}`
+        );
+        if (!res.ok) throw new Error(`HTTP ${res.status}`);
+        const list = await res.json();
+        this.count = list.count;
+        console.log("List: ", list);
+
+        const details = await Promise.all(
+          list.results.map((animal) => fetch(animal.url).then((r) => r.json()))
+        );
+
+        console.log("Details: ", details);
+
+        this.items = details.map((d) => ({
+          pid: d.id,
+          name: d.name,
+          img:
+            d.sprites?.other?.["official-artwork"]?.front_default ||
+            d.sprites?.front_default ||
+            "",
+          types: d.types.map((t) => t.type.name),
+          stats: Object.fromEntries(
+            d.stats.map((s) => [s.stat.name, s.base_stat])
+          ),
+        }));
+      } catch (err) {
+        this.innerHTML = `<p style="color:red">Error loading Pokédex: ${String(
+          err
+        )}</p>`;
+      }
+    }
+    this.render();
+    this._bindSearch();
+    this._bindPager();
   }
   render() {
     const items = this.items ?? [];
@@ -101,6 +177,17 @@ export default class Pokedex extends HTMLElement {
     this.innerHTML = `
         <section>
             <h2>Pokedex</h2>
+
+            <form id="search-form">
+                <input 
+                    id="search-input"
+                    type="search"
+                    placeholder="Search name or #id"
+                >
+                <button type="submit">Search</button>
+                <button id="clear-search">Clear</button>
+            </form>
+
             <div class="pokedex-grid">
                 ${items
                   .map(
@@ -129,6 +216,7 @@ export default class Pokedex extends HTMLElement {
                 <button id="next">Next ></button>
                 <button id="extra-next">+10 ></button>
             </div>
+               
         </section>
     `;
   }
